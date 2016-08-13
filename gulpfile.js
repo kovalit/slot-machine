86b5b5de859558526bdeff6cc654773e182b9212
kovalit <kovalit@mail.ru>
2016-08-13 18:07:04 +1000

    added scss styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,43 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var gulp = require('gulp'), 
+    sass = require('gulp-sass'), 
+    cleanCSS = require('gulp-clean-css'),
+    concat = require('gulp-concat'),
+    watch = require('gulp-watch'),
+    rename = require('gulp-rename'),
+    cssImageDimensions = require("gulp-css-image-dimensions"),
+    mainBowerFiles = require('main-bower-files'),
+    uglify = require('gulp-uglify'),
+    rigger = require('gulp-rigger');
 
-var gulp = require('gulp');
 
-gulp.task('default', function () {
-    // place code for your default task here
+gulp.task('sass', function(){ 
+    return gulp.src('public/css/scss/**/*.scss') 
+        .pipe(sass({
+            outputStyle: 'expanded'
+        }))
+        .pipe(cssImageDimensions('../../img'))
+        .pipe(gulp.dest('public/css/'))
+        .pipe(rename({ extname: '.min.css' }))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('public/css/')) 
 });
+
+gulp.task('watch', function(){
+    watch(['public/css/**/*.scss'], function(event, cb) {
+        gulp.start('sass');
+    });
+});
+
+gulp.task('bower', function() {
+    return gulp.src(mainBowerFiles({ filter:'**/*.js'}))
+        .pipe(gulp.dest('public/js/dist'))
+});
+
+gulp.task('js', function() {
+    return gulp.src('public/js/dist/main.js')
+        .pipe(rigger())
+        .pipe(uglify())
+        .pipe(gulp.dest('public/js'))
+});
+
+gulp.task('default', ['sass', 'watch']);
